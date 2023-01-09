d9364b8222882f6f68f99fcdaf21597f56e67e4a
Binaya Maharjan <wpcirqle@gmail.com>
2023-01-09 14:42:27 +0545

    Worked on responsive

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 import "./src/scss/style.scss"
 
+import $ from "jquery"
+
 import "sticky-sidebar"
 
-var sidebar = new StickySidebar('.sidebar', {
-  topSpacing: 20,
-  bottomSpacing: 20,
-});
+if ($(window).width() > 550) {
+  var sidebar = new StickySidebar(".sidebar", {
+    topSpacing: 20,
+    bottomSpacing: 20,
+  })
+}
+
+$(".toggle").on("click", function () {
+  $(this).prev("ul").slideToggle()
+  $(this).toggleClass("active")
+})
